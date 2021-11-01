df26683795f65323ed961522f786831e78e478f0
Aleks <aleksskela591@gmail.com>
2021-11-01 11:12:14 +0100

    Hello World

--- src/pl/java/helloworld/HelloWorld.java
+++ src/pl/java/helloworld/HelloWorld.java
@@ -1,16 +1,8 @@
 package pl.java.helloworld;
 
-import javax.swing.JOptionPane;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
-
-    String name = JOptionPane.showInputDialog("Enter your name");
-    JOptionPane.showMessageDialog(null, "Hello "+name);
-
-    int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-    JOptionPane.showMessageDialog(null, "You are "+age+" years old");
-
+        System.out.println("Hello World");
     }
 }
