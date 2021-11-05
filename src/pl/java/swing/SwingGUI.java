630573ed55f986af6ffcc10196dab311b576b13f
Aleks <aleksskela591@gmail.com>
2021-11-05 15:05:05 +0100

    Swing GUI workn

--- src/pl/java/swing/SwingGUI.java
+++ src/pl/java/swing/SwingGUI.java
@@ -1,17 +1,30 @@
 package pl.java.swing;
-
 import javax.swing.*;
-
 public class SwingGUI {
 
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello " + name);
 
-        String inputAge = JOptionPane.showInputDialog("Enter your age");
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, "Hello " + name + "!");
+
+        String age;
+        boolean isNotDigit = false;
+        do {
+            age = (JOptionPane.showInputDialog("Enter your age: "));
+            for (char c : age.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    JOptionPane.showMessageDialog(null, "You are stupid!");
+                    isNotDigit = true;
+                    break;
+                }
+            }
+        } while (isNotDigit);
+        JOptionPane.showMessageDialog(null, "Your age is: " + age);
 
 
-        int age = Integer.parseInt("2");
-        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
     }
+
+
 }
+
+
