2d554f00a00ba9dc124cb7feebfa434d3c3b9687
Aleks <aleksskela591@gmail.com>
2021-11-08 15:47:31 +0100

    logical operators

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -1,33 +1,46 @@
 package Learning;
 
+import java.util.Scanner;
+
 public class YtLearning {
     public static void main(String[] args) {
-        // switch = statement that allows a variable to be tested for equality against a list of values
-        // switch works with ( byte, short, char, int) it also works witch enumerated types ( the String class, and few classes that wrap certain primitive types: Character, Byte, Short, and Integer
 
-        String day = "pizza";
+        // logical operators = used two or more expressions
 
-        switch (day){
-            case "Monday":  System.out.println("It is Monday!");
-                break;
-            case "Tuesday":  System.out.println("It is Tuesday!");
-                break;
-            case "Wednesday":  System.out.println("It is Wednesday!");
-                break;
-            case "Thursday":  System.out.println("It is Thursday!");
-                break;
-            case "Friday":  System.out.println("It is Friday!");
-                break;
-            case "Saturday":  System.out.println("It is Saturday!");
-                break;
-            case "Sunday":  System.out.println("It is Sunday!");
-                break;
-            default: System.out.println("That is not a day!!!");
+        //                      && = (AND) both conditions must be TRUE
+        //                      || = (OR) either conditions must be TRUE
+        //                      ! = (NOT) reverses boolean value of condition
+
+        int temp = 15;      //&&
+        if (temp > 30) {
+            System.out.println("It is hot outside");
+        } else if (temp >= 20 && temp <= 30) {
+            System.out.println("It is warm outside");
+        } else {
+            System.out.println("It is cold outside");
         }
 
+        Scanner scanner = new Scanner(System.in);
+        // ||
 
+        System.out.println("You are playing a game! Press q or Q to quit!");
+        String response = scanner.next();
+
+        if (response.equals("q") || response.equals("Q")) {
+            System.out.println("You quit the game.");
+        } else {
+            System.out.println("You are still playing the game *pew pew*");
+        }
+
+        // !
+        if (!response.equals("q") && !response.equals("Q")) {
+            System.out.println("You still wants to play this game.");
+        } else {
+            System.out.println("You quited the game");
+        }
     }
-
 }
+
+
 
 
