76a8b27ff6922007b36fabf9b86fd9e1a67173ff
Aleks <aleksskela591@gmail.com>
2021-11-08 16:28:18 +0100

    nested loops

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -1,14 +1,30 @@
 package Learning;
 
+import java.util.Scanner;
+
 public class YtLearning {
     public static void main(String[] args) {
 
-        // for loop = executes  block of code limited amount of times
+        // nested loops = a loop inside of a loop
 
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+
+        int rows, columns;
+        String symbol = "";
+
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.println();
+            for (int j = 1; j <= columns; j++) {
+                System.out.print(symbol);
+            }
         }
-        System.out.println("Happy New Year!!! ");
     }
 }
 
