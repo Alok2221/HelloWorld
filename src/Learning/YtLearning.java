c3315b47b68e9b75b1d0d4465be33ea127641d55
Aleks <aleksskela591@gmail.com>
2021-11-08 16:03:10 +0100

    while loop

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -5,39 +5,17 @@ import java.util.Scanner;
 public class YtLearning {
     public static void main(String[] args) {
 
-        // logical operators = used two or more expressions
-
-        //                      && = (AND) both conditions must be TRUE
-        //                      || = (OR) either conditions must be TRUE
-        //                      ! = (NOT) reverses boolean value of condition
-
-        int temp = 15;      //&&
-        if (temp > 30) {
-            System.out.println("It is hot outside");
-        } else if (temp >= 20 && temp <= 30) {
-            System.out.println("It is warm outside");
-        } else {
-            System.out.println("It is cold outside");
-        }
+        // while loop = executes a block of code as long as it's condition remains true
 
         Scanner scanner = new Scanner(System.in);
-        // ||
+        String name = "";
 
-        System.out.println("You are playing a game! Press q or Q to quit!");
-        String response = scanner.next();
-
-        if (response.equals("q") || response.equals("Q")) {
-            System.out.println("You quit the game.");
-        } else {
-            System.out.println("You are still playing the game *pew pew*");
+        do {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
         }
-
-        // !
-        if (!response.equals("q") && !response.equals("Q")) {
-            System.out.println("You still wants to play this game.");
-        } else {
-            System.out.println("You quited the game");
-        }
+        while (name.isBlank());
+        System.out.println("Hello " + name);
     }
 }
 
