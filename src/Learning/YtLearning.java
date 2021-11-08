f490a414f134de4ea4548ea940484d9a632fff92
Aleks <aleksskela591@gmail.com>
2021-11-08 16:42:05 +0100

    arrays no.2

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -5,10 +5,17 @@ public class YtLearning {
 
         // array = used to store multiple values in single variable
 
-        String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+        String[] cars = new String[3];
 
-        cars[0] = "Mustang";
-        System.out.println(cars[3]);
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+
     }
 }
 
