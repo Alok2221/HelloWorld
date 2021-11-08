c5700452240e452d9777098f69d51e7cd1af8fd1
Aleks <aleksskela591@gmail.com>
2021-11-08 16:53:48 +0100

    2D arrays no.1

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -3,18 +3,27 @@ package Learning;
 public class YtLearning {
     public static void main(String[] args) {
 
-        // array = used to store multiple values in single variable
+        // 2D arrays = an array of arrays
 
-        String[] cars = new String[3];
+        String[][] cars = new String[3][3];
 
-        cars[0] = "Camaro";
-        cars[1] = "Corvette";
-        cars[2] = "Tesla";
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "Camaro";
+        cars[2][1] = "Lambo";
+        cars[2][2] = "Tesla";
 
         for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
+            System.out.println();
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print(cars[i][j] + " ");
+            }
         }
-
 
     }
 }
