0daab0e8c2e742eb2ae8560ff479707925d0947f
Aleks <aleksskela591@gmail.com>
2021-11-08 15:20:45 +0100

    switches

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -1,16 +1,33 @@
 package Learning;
 
-import javax.swing.JOptionPane;
-
 public class YtLearning {
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello " + name);
+        // switch = statement that allows a variable to be tested for equality against a list of values
+        // switch works with ( byte, short, char, int) it also works witch enumerated types ( the String class, and few classes that wrap certain primitive types: Character, Byte, Short, and Integer
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+        String day = "pizza";
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
+        switch (day){
+            case "Monday":  System.out.println("It is Monday!");
+                break;
+            case "Tuesday":  System.out.println("It is Tuesday!");
+                break;
+            case "Wednesday":  System.out.println("It is Wednesday!");
+                break;
+            case "Thursday":  System.out.println("It is Thursday!");
+                break;
+            case "Friday":  System.out.println("It is Friday!");
+                break;
+            case "Saturday":  System.out.println("It is Saturday!");
+                break;
+            case "Sunday":  System.out.println("It is Sunday!");
+                break;
+            default: System.out.println("That is not a day!!!");
+        }
+
+
     }
+
 }
+
+
