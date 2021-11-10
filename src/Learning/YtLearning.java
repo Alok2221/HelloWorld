adec4e3b85d6550162bbf603fad432b8cfe0c00a
Aleks <aleksskela591@gmail.com>
2021-11-10 13:36:49 +0100

    ArrayList

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -1,32 +1,28 @@
 package Learning;
 
+import java.util.ArrayList;
+
 public class YtLearning {
+
     public static void main(String[] args) {
 
-        // wrapper class = provides a way to use primitive data types as reference data types
-        //                 reference data types contain useful methods
-        //                 can be used with collections (ex. ArrayList)
-        // primitive data types are much faster
+        // ArrayList = a resizable array.
+        //             Elements can be added and removed after compilation phase
+        //             store reference data types
 
+        ArrayList<String> food = new ArrayList<String>();
 
-        // primitive        // wrapper
-        // ---------        // -------
-        // boolean          // Boolean
-        // char             // Character
-        // int              // Integer
-        // double           // Double
+        food.add("pizza");
+        food.add("hamburger");
+        food.add("hotdog");
 
-        // autoboxing = the automatic conversion that Java compiler makes between the primitive types and their corresponding object wrapper classes
-        // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+        food.set(0, "sushi");
+        food.remove(2);
+        food.clear();
 
-        Boolean a = true;
-        Character b = '@';
-        Integer c = 123;
-        Double d = 3.14;
-        String e = "Aleks";
-
-        if (a == true) {
-            System.out.println("This is true");
+        for (int i = 0; i < food.size(); i++) ;
+        {
+            System.out.println(food);
         }
     }
 }
