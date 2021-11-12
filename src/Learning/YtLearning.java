2e69416a069c27bfd856393be55567a869e94124
Aleks <aleksskela591@gmail.com>
2021-11-12 18:09:07 +0100

    printf

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -3,42 +3,44 @@ package Learning;
 public class YtLearning {
 
     public static void main(String[] args) {
+        // printf() =   an optional method to control, format, and display text to the console window
+        //              two arguments = format string + (object/variable/value)
+        //              % [flags][precision][width][conversion-character]
 
-        // overloaded methods = methods that share same name but have different parameters
-        //                      method name + parameter = method signature
+        boolean myBoolean = true;       // %b for boolean
+        char myChar = '@';              // %c for char
+        String myString = "Aleks";      // %s for String
+        int myInt = 50;                 // %d for int
+        double myDouble = 1000.99;      // %f for double, float
 
-        double x = add(1.0, 4.0, 7.0, 10.0);
-        System.out.println(x);
+        System.out.printf("%b ", myBoolean);
+        System.out.println("");
+        System.out.printf("%c ", myChar);
+        System.out.println("");
+        System.out.printf("%s ", myString);
+        System.out.println("");
+        System.out.printf("%d ", myInt);
+        System.out.println("");
+        System.out.printf("%f ", myDouble);
+        System.out.println("");
 
-    }
+        //[width] = minimum number of characters to be witten as output
 
-    static int add(int a, int b) {
-        System.out.println("This is overloaded methode #1");
-        return a + b;
-    }
+        System.out.printf("Hello%7s", myString);
+        System.out.println("");
 
-    static int add(int a, int b, int c) {
-        System.out.println("This is overloaded methode #2");
-        return a + b + c;
-    }
+        //[precision] = sets number of digits of precision when outputting floating-point values
 
-    static int add(int a, int b, int c, int d) {
-        System.out.println("This is overloaded methode #3");
-        return a + b + c + d;
-    }
+//      System.out.printf("You have this much money %.2f ", myDouble);
+        System.out.println("");
 
-    static double add(double a, double b) {
-        System.out.println("This is overloaded methode #4");
-        return a + b;
-    }
+        // [flags] = adds an effect to output based on the flag added to format specifier
+        // - : left-justify
+        // + : output a plus ( + ) or minus ( - ) sign for a numeric value
+        // 0 : numeric values are zero-padded
+        // , : comma grouping separators if number > 1000
 
-    static double add(double a, double b, double c) {
-        System.out.println("This is overloaded methode #5");
-        return a + b + c;
-    }
+        System.out.printf("You have this much money %.2f", myDouble);
 
-    static double add(double a, double b, double c, double d) {
-        System.out.println("This is overloaded methode #6");
-        return a + b + c + d;
     }
 }
