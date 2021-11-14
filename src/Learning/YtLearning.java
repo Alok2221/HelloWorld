8de944d7b98e06816937d75d90d3b697b54c4074
Aleks <aleksskela591@gmail.com>
2021-11-14 14:56:27 +0100

    objects (OOP)

--- src/Learning/YtLearning.java
+++ src/Learning/YtLearning.java
@@ -5,8 +5,22 @@ public class YtLearning {
 
     public static void main(String[] args) {
 
-        final double PI = 3.14169;
-        System.out.println(PI);
+        // object = an instance of a class that may contain attributes and methods
+        //          example: (phone, desk, computer, coffee cup)
 
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+
+        System.out.println(myCar1.price);
+        System.out.println(myCar1.model);
+        System.out.println(myCar1.color);
+        System.out.println("");
+        System.out.println(myCar2.price);
+        System.out.println(myCar2.model);
+        System.out.println(myCar2.color);
+
+//        myCar.drive();/*
+//        myCar.brake();
     }
 }
+
