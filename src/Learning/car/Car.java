9bb0c3ab7a6d73a8e62c0c267b50abc030676181
Aleks <aleksskela591@gmail.com>
2022-01-31 11:48:04 +0100

    object passing

---
+++ src/Learning/car/Car.java
@@ -0,0 +1,9 @@
+package Learning.car;
+
+public class Car {
+ String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Learning/car/Garage.java
@@ -0,0 +1,8 @@
+package Learning.car;
+
+public class Garage {
+
+    public void park(Car car) {
+        System.out.println("The " + car.name + " is parked in the garage");
+    }
+}
