0daacec3d892babf9bb64d136012cfee80371d5f
Aleks <aleksskela591@gmail.com>
2022-02-01 13:48:52 +0100

    file class

---
+++ src/Learning/fileclass/HowToFile.java
@@ -0,0 +1,20 @@
+package Learning.fileclass;
+
+import java.io.File;
+
+public class HowToFile {
+    public static void main(String[] args) {
+
+        //  file = An abstract representation of file and directory pathname
+
+        File file = new File("C:/Users/aleks/Desktop/Projekty/HelloWorld/src/Learning/fileclass/secret_message.txt");
+
+        if (file.exists()) {
+            System.out.println("That file exists! :O");
+            System.out.println(file.getPath());
+            System.out.println(file.isFile());
+        } else {
+            System.out.println("That file doesn't exist :(");
+        }
+    }
+}
